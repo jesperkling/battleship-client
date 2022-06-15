8e0a566a65c064a4123413c9e79ee24ecb119dd4
Jesper Kling <63609498+jesperkling@users.noreply.github.com>
2022-06-15 22:27:07 +0200

    Added user form on start page and it's working to register username.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
 import socketio from 'socket.io-client'
 import './App.css';
+import { useEffect, useState } from 'react'
 
 const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL)
 
 const App = () => {
 	console.log(socket)
+
+	const [username, setUsername] = useState('')
+	const [userInput, setUserInput] = useState('')
+
+	const handleUsernameSubmit = e => {
+		e.preventDefault()
+		setUsername(userInput)
+		socket.emit('player:username', userInput)
+		setUserInput('')
+	}
+
+	useEffect(() => {
+		console.log(username)
+	}, [username])
+
 	return (
 		<div>
 			<h1>Battleship Game</h1>
+			<div className='start-page'>
+				<form onSubmit={handleUsernameSubmit}>
+					<div>
+						<input
+							type='text'
+							id='username'
+							required
+							placeholder='Username'
+							value={userInput}
+							onChange={e => setUserInput(e.target.value)}
+						/>
+						<button type='submit'>Start Game</button>
+					</div>
+				</form>
+			</div>
 		</div>
 	)
 }
