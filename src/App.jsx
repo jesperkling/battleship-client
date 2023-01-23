a15ed7e21149f6e6b86f7eae0f95411b17b96b71
Jesper Kling <jesperkling87@gmail.com>
2023-01-23 12:01:27 -0500

    Added react router and GamePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+import GamePage from "./Pages/GamePage";
+import Container from "react-bootstrap/Container";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      Battleship Game
+      <Container>
+        <Routes>
+          <Route path="/gameboard" element={<GamePage />} />
+        </Routes>
+      </Container>
     </div>
   );
 }
