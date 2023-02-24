61b12b279ce887df35ad9049afa1a29301afb1e4
Jesper Kling <jesperkling87@gmail.com>
2023-02-24 16:33:23 +0100

    Cleaned up App.css and added id to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="App">
       <SocketContext>
         <Routes>
           <Route path="*" element={<StartPage />} />
