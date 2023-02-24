b3bb8b156b9c8d6b1c86081dc1d11cc73db48a48
Jesper Kling <jesperkling87@gmail.com>
2023-02-24 16:45:41 +0100

    Imported styling from separate css file

--- src/Pages/StartPage.jsx
+++ src/Pages/StartPage.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
+import "../Assets/Style/StartPage.css";
 
 // Router
 import { Link } from "react-router-dom";
